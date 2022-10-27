e8cf733b48b6f70f855d76f7202ab78671f5eb31
ekakurniaa <ekakurrr@gmail.com>
2022-10-27 11:32:10 +0700

    Pertemuan 2-6

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php 
+class Contoh1 extends CI_Controller {
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Eka Kurnia
+            Saya tinggal di daerah Bekasi
+            Olahraga yang saya sukai adalah Taekwondo";         
+    }
+}
+
+
+?>
